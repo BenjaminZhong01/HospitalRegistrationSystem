c6e34cdd92b07a9bea01e69e2b08fda12043b85c
Benjamin Zhong <renjiebenjamin@gmail.com>
2022-06-29 21:09:44 +0930

    fix: fixed all bugs up to P17 --day2

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -2,13 +2,14 @@ package com.atguigu.yygh.hosp.controller;
 
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "Hospital Setup Management")
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
@@ -16,9 +17,17 @@ public class HospitalSetController {
     @Autowired
     private HospitalSetService hospitalSetService;
 
+    @ApiOperation(value = "Obtain page lists")
     @GetMapping("findAll")
     public List<HospitalSet> findAllHospitalSet(){
         List<HospitalSet> list = hospitalSetService.list();
         return list;
+    }
+
+    @ApiOperation(value = "Logically delete hospital setup")
+    @DeleteMapping("{id}")
+    public boolean removeHospSet(@PathVariable Long id) {
+        boolean flag = hospitalSetService.removeById(id);
+        return flag;
     }
 }
