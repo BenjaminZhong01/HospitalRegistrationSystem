fa629f1472eb353cedbded9fe35d0e19889a7543
Benjamin Zhong <renjiebenjamin@gmail.com>
2022-07-06 15:22:10 +0930

    feat: add search data dictionary function [videos 59, day 6]

---
+++ service/service_cmn/src/main/java/com/atguigu/yygh/cmn/controller/DictController.java
@@ -0,0 +1,29 @@
+package com.atguigu.yygh.cmn.controller;
+
+import com.atguigu.yygh.cmn.service.DictService;
+import com.atguigu.yygh.common.result.Result;
+import com.atguigu.yygh.model.cmn.Dict;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(value = "Data Dictionary API")
+@RestController
+@RequestMapping("/admin/cmn/dict")
+@CrossOrigin
+public class DictController {
+
+    @Autowired
+    private DictService dictService;
+
+    //search children list by id
+    @ApiOperation(value = "search children list by id")
+    @GetMapping("findChildData/{id}")
+    public Result findChildData(@PathVariable Long id) {
+        List<Dict> list = dictService.findChildData(id);
+        return Result.ok(list);
+    }
+}
---
+++ service/service_cmn/src/main/java/com/atguigu/yygh/cmn/service/impl/DictServiceImpl.java
@@ -0,0 +1,37 @@
+package com.atguigu.yygh.cmn.service.impl;
+
+import com.atguigu.yygh.cmn.mapper.DictMapper;
+import com.atguigu.yygh.cmn.service.DictService;
+import com.atguigu.yygh.model.cmn.Dict;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements DictService {
+
+    //search children list by id
+    @Override
+    public List<Dict> findChildData(Long id) {
+        QueryWrapper<Dict> wrapper = new QueryWrapper<>();
+        wrapper.eq("parent_id", id);
+        List<Dict> dictList = baseMapper.selectList(wrapper);
+        //set hasChildren value
+        for(Dict dict: dictList) {
+            Long dictId = dict.getId();
+            boolean isChild = this.isChildren(dictId);
+            dict.setHasChildren(isChild);
+        }
+        return dictList;
+    }
+
+    //check if id has children
+    private boolean isChildren(Long id) {
+        QueryWrapper<Dict> wrapper = new QueryWrapper<>();
+        wrapper.eq("parent_id", id);
+        Integer count = baseMapper.selectCount(wrapper);
+        return count > 0;
+    }
+}
