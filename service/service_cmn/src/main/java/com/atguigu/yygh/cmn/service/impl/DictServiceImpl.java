af7de83dca2b3e9a23e73ded37cbe234f8f98ea5
Benjamin Zhong <renjiebenjamin@gmail.com>
2022-07-13 18:46:14 +0930

    day 6

--- service/service_cmn/src/main/java/com/atguigu/yygh/cmn/service/impl/DictServiceImpl.java
+++ service/service_cmn/src/main/java/com/atguigu/yygh/cmn/service/impl/DictServiceImpl.java
@@ -11,6 +11,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.fasterxml.jackson.databind.util.BeanUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
